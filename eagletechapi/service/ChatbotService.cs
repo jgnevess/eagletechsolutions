779a394a581a2bcfe157ee11a9e7e9964dad2019
Joao Gabriel <joaogabriel443@gmail.com>
2025-08-07 11:17:54 -0300

    Instrução para o chatbot

--- eagletechapi/service/ChatbotService.cs
+++ eagletechapi/service/ChatbotService.cs
@@ -10,7 +10,7 @@ namespace eagletechapi.services
     public class ChatbotService
     {
         private readonly string API_KEY;
-    
+
 
         private readonly ClientHttp _http;
 
@@ -22,31 +22,41 @@ namespace eagletechapi.services
 
         public async Task<IEnumerable<Message>> Conversation(ChatbotPart chatbotPart)
         {
-
+            
             Chatbot.Conversation.Add(new Message
             {
                 MessageType = MessageType.SENT,
                 MessageText = chatbotPart.text
             });
 
-
             var chatbotIn = new ChatbotIn();
             var chatbotContent = new ChatbotContent();
+
+        
+            if (Chatbot.Conversation.Count == 1)
+            {
+                chatbotContent.parts.Add(new ChatbotPart
+                {
+                    text = "Você é um membro da equipe de suporte técnico de informática. Responda com frases curtas, diretas lembrando sempre que o usuário pode ser leigo no assunto."
+                });
+            }
+
+        
             chatbotContent.parts.Add(chatbotPart);
             chatbotIn.contents.Add(chatbotContent);
 
-
+        
             var response = await _http.Enviar(
                 "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent",
                 chatbotIn,
                 API_KEY
             );
 
-
+    
             var chatbotOut = await response.Content.ReadFromJsonAsync<ChatbotOut>();
             var respostaTexto = chatbotOut?.candidates?.FirstOrDefault()?.content?.parts?.FirstOrDefault()?.text;
 
-
+    
             if (!string.IsNullOrWhiteSpace(respostaTexto))
             {
                 Chatbot.Conversation.Add(new Message
@@ -58,6 +68,7 @@ namespace eagletechapi.services
 
             return Chatbot.Conversation;
         }
+
 
     }
 }
